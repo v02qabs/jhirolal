7d80817bc9d47a03564eac1dee11e4968b0ad5b9
v02qabs <takesue090@gmail.com>
2024-01-18 14:27:57 +0900

    hiro-value-and-method-testing-methoda

--- test-lang/hirolang.java
+++ test-lang/hirolang.java
@@ -4,6 +4,9 @@ class jhirolang{
 	public static void main(String[] args)
 	{
 		System.out.println("Hello test-lang");
+		new jhirolang();
 	}
+	private void setting_method(String method_name){
+		System.out.println
 }
 
---
+++ test-lang/local-method.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+class hlvalue{
+	public hlvalue(){
+	}
+	public hlvalue(String method_name){
+		
+	}
+	public void show(String method_name){
+		System.out.println(method_name);
+
